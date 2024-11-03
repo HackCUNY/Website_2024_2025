867feabb4bdaa50d345bad98b3107577a97e4e30
Leandro Gamarra <leandrogamarra@rocketmail.com>
2024-11-03 13:54:01 -0500

    previous hackathon setup

--- Client/src/Pages/home.jsx
+++ Client/src/Pages/home.jsx
@@ -3,25 +3,20 @@ import styled from 'styled-components';
 import Hero from '../Components/hero';
 import About from '../Components/about';
 import Registration from '../Components/registration';
-
-// For CSS styling, follow format below to prevent mergin conflict.
-// Make sure you only code on your designated page. Don't Change any other file.*
-// delete below when done
-const Example_div = styled.div`
-  color: red;
-`
-
+import Previous_Hackathon from '../Components/previous_hackathon';
 
 function Home() {
   return (
     // Import Hero Section First
     // Import About Section Second
     // Import Registration Section Third
+    // Import Previous Hackathon Section Fourth
     // In this order
     <div >
       <Hero/>
       <About/>
       <Registration/>
+      <Previous_Hackathon/>
     </div>
   )
 }
