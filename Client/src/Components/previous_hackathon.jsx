58b11c24cef3d008d96379910edc9f6a39336aa2
Leandro Gamarra <leandrogamarra@rocketmail.com>
2024-12-06 00:20:38 -0500

    Updates

--- Client/src/Components/previous_hackathon.jsx
+++
@@ -1,8 +0,0 @@
-// import React from 'react'
-import styled from 'styled-components';
-
-export default function Previous_Hackathon() {
-  return (
-    <div>previous_hackathon section</div>
-  )
-}
